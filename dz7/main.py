8fdc62aa98b3b10b70eca5a2d1b2677a7548a243
Gowaa <gowaa@bk.ru>
2023-06-20 00:40:51 +0300

    edit task2

--- dz7/main.py
+++ dz7/main.py
@@ -1,6 +1,6 @@
 # 2. Соберите из созданных на уроке и в рамках домашнего задания функций пакет для работы с файлами.
 
-from work_files import rename_group
+from work_files import rename_group, name_gen
 
 if __name__ == '__main__':
     rename_group.group_rename(5, 'rtf', 'txt', [2, 6], "somefilename")
